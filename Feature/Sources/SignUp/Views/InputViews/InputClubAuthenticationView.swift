63d00b3488b550c86ef8e0d60f65866a1f2e45c0
JunnKyuu <junnkyuu22@gmail.com>
2025-09-26 09:12:35 +0900

    fix: 회원가입 학번 입력 number pad로 수정

--- Feature/Sources/SignUp/Views/InputViews/InputClubAuthenticationView.swift
+++ Feature/Sources/SignUp/Views/InputViews/InputClubAuthenticationView.swift
@@ -48,7 +48,7 @@ struct InputClubAuthenticationView: View {
                     Text("학번")
                         .font(.custom("GmarketSansMedium", size: 16))
                         .foregroundStyle(Color("gray_90"))
-                    SignUpTextField(hint: "60221234", value: $studentNum)
+                    SignUpNumberPad(hint: "60221234", value: $studentNum)
                 }
                 .padding(.bottom, 20)
                 
